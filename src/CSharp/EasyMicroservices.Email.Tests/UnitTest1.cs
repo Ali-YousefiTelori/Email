b403b66a0ba34731e888412220b199499735c239
Ali Yousefi <ali.visual.studio@gmail.com>
2023-05-17 11:09:14 +0330

    add fir initialization

---
+++ src/CSharp/EasyMicroservices.Email.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EasyMicroservices.Email.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/CSharp/EasyMicroservices.Email/Interfaces/IEmailProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyMicroservices.Email.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IEmailProvider
+    {
+    }
+}
